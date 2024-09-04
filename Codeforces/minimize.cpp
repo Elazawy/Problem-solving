879a8f7588d2dd7450ef13262d474397181f9d9b
Elazawy <ahmedsaadelazawy222@gmail.com>
2024-09-04 09:43:45 +0300

    upload new problem

---
+++ Codeforces/minimize.cpp
@@ -0,0 +1,20 @@
+// Competation: Codeforces Round 971 (Div. 4)
+// Problem name: Minimize!
+// URL : https://codeforces.com/contest/2009/problem/A
+// Memory limit : 256
+// Time limit : 1000
+// Date: Mon Sep  3 2024 
+
+// Used libraries
+#include<iostream>
+using namespace std;
+int main(){
+    int num_cases;
+    cin >> num_cases;
+    while (num_cases--)
+    {
+        int a,b;
+        cin >> a >> b;
+        cout << (b-a) << endl;
+    }
+}
