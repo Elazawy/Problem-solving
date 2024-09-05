5de513431ab2a773d12b08c82e0f78b8f94d5d40
Elazawy <ahmedsaadelazawy222@gmail.com>
2024-09-05 16:20:49 +0300

    Upload the problem Divisible

---
+++ Codeforces/Divisible.cpp
@@ -0,0 +1,26 @@
+// Problem name: M. Divisible
+// URL : https://codeforces.com/group/MWSDmqGsZm/contest/223338/problem/M
+// Memory limit : 256
+// Time limit : 1000
+// Date: Mon Sep  3 2024 
+
+// Used libraries
+#include<iostream>
+using namespace std;
+
+
+int main(){
+    string N;
+    int x;
+    cin >> N >> x;
+    long long remindar = 0;
+    for(char digit : N){
+        remindar = (remindar*10 + (digit - '0')) % x;
+    }
+    if(remindar==0) {
+        cout << "YES";
+    }
+    else {
+        cout << "NO";
+    }
+}
