f99a4684730b80d69a68a7345247cc3c200f3fcf
Elazawy <ahmedsaadelazawy222@gmail.com>
2024-09-09 09:01:00 +0300

    Upload the problem Big add and multiply

---
+++ Codeforces/Big_Add_and_Multiply.py
@@ -0,0 +1,7 @@
+# Problem name : Big Add and Multiply
+# URL : https://codeforces.com/group/MWSDmqGsZm/contest/223338/problem/O
+Date : sun sep 8 2024
+
+num = int(input())
+print(num + 9999)
+print(num * 9999)
